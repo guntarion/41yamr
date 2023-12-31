7cd0325787f08ecdea9be750058fefe23a86a5e7
guntar <guntar@me.com>
2023-12-31 13:56:59 +0700

    Add 'unitbidang' app to INSTALLED_APPS

---
+++ yamr2024/unitbidang/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Bidang, Unit
+
+# Register your models here.
+admin.site.register(Bidang)
+admin.site.register(Unit)
---
+++ yamr2024/unitbidang/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Bidang(models.Model):
+    namaBidang = models.CharField(max_length=200)
+    kodeBidang = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return self.namaBidang
+
+
+class Unit(models.Model):
+    namaUnit = models.CharField(max_length=200)
+    kodeUnit = models.CharField(max_length=10)
+    bidang = models.ForeignKey(Bidang, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.namaUnit
